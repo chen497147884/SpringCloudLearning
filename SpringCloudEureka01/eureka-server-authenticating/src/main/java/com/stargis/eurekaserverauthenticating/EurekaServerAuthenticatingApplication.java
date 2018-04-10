9f6f53724c4dba1c3ac50a63d3253024facfb300
ForeverChen <497147884@qq.com>
2018-04-10 20:27:39 +0800

    2018年4月10日20:27:31

---
+++ SpringCloudEureka01/eureka-server-authenticating/src/main/java/com/stargis/eurekaserverauthenticating/EurekaServerAuthenticatingApplication.java
@@ -0,0 +1,12 @@
+package com.stargis.eurekaserverauthenticating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerAuthenticatingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerAuthenticatingApplication.class, args);
+    }
+}
