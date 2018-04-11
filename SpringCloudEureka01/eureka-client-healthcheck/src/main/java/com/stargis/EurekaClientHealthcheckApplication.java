0c2bf5ed73e3d305b5641d45c2f07415b264cde6
ForeverChen <497147884@qq.com>
2018-04-11 21:27:16 +0800

    2018年4月11日21:27:12

---
+++ SpringCloudEureka01/eureka-client-healthcheck/src/main/java/com/stargis/EurekaClientHealthcheckApplication.java
@@ -0,0 +1,14 @@
+package com.stargis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class EurekaClientHealthcheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientHealthcheckApplication.class, args);
+    }
+}
---
+++ SpringCloudEureka01/eureka-client-healthcheck/src/test/java/com/stargis/EurekaClientHealthcheckApplicationTests.java
@@ -0,0 +1,16 @@
+package com.stargis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaClientHealthcheckApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
